5d4904c89b474da63baa07caa405bb33bdd40a33
JALLI SAMUEL PAUL <samuelpauljalli@gmail.com>
2026-09-11 11:34:58 +0530

    Update comment for Firebase configuration file

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-// Central Firebase Configuration for SPEK Tech Superstore
+// Central Firebase Configuration for SPOKE Tech Superstore
 // Uses Firebase modular JavaScript SDK with ES modules for direct browser integration
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
